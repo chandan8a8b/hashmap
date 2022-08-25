d08bc8832591b7bde99781f9640b75a5f6a63fe5
chandan8a8b <112074602+chandan8a8b@users.noreply.github.com>
2022-08-25 14:45:17 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,13 @@
+package app_java_10;
+
+public class A {
+	static int x1 = 10;
+	
+	public static void main(String arg[]) {
+		int x2 = 20;
+		System.out.println(A.x1);
+		System.out.println(x2);
+		
+	}
+
+}
